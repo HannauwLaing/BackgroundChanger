d9cc5bb688ed79c8a7de8766a6fcf9325fc0e96a
Hannauw Laing <27086534@sun.ac.za>
2025-11-19 09:37:38 +0200

    Fixed a few bugs added in previous commit

--- change-background.sh
+++ change-background.sh
@@ -3,16 +3,13 @@
 #Source of images:
 # https://github.com/D3Ext/aesthetic-wallpapers/tree/main/images
 
+SETTINGSFILE="Settings.txt"
 # Directory containing your images
-IMAGE_DIR="bin/wallpapers"
-
-# Type of wallpaper
-# cosmic,
+CUR_DIR=$(pwd)
+IMAGE_DIR=$CUR_DIR/$(sed -n '2p' "$SETTINGSFILE")
 TYPE=''
-
 FILE=$(find "$IMAGE_DIR" -type f \( -iname '*.jpg' -o -iname '*.jpeg' -o -iname '*.png' -o -iname '*.gif' -o -iname '*.bmp' -o -iname '*'+$TYPE+'*' \) | shuf -n 1)
 
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$FILE"
-
+gsettings set org.gnome.desktop.background picture-uri-dark $FILE
 exit
 
--- changebackgroundLoop.sh
+++ changebackgroundLoop.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #Defaults
 SETTINGSFILE="Settings.txt"
-SLEEPTIME=15
+# SLEEPTIME=15
+CUR_DIR=$(pwd)
+IMAGE_DIR="$CUR_DIR/$(sed -n '2p' "$SETTINGSFILE")"
+SLEEPTIME=$(($(sed -n '4p' "$SETTINGSFILE")))
+FIRSTFILE="$IMAGE_DIR/$(sed -n '6p' "$SETTINGSFILE")"
 
-# SETTINGSFILE=$(sed -n '1p' "$SETTINGSFILE")
-SLEEPTIME=$(($(sed -n '2p' "$SETTINGSFILE")))
-FIRSTFILE=$(($(sed -n '3p' "$SETTINGSFILE")))
-
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$FIRSTFILE"
+gsettings set org.gnome.desktop.background picture-uri-dark "$FIRSTFILE"
 
 while true; do
 	sleep $((SLEEPTIME))
