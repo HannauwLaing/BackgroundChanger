0f320a4f18a2618627c3c754db046073148e379e
Hannauw Laing <27086534@sun.ac.za>
2025-11-20 12:57:00 +0200

    Made change-backgroung run from any dir aswell

--- changebackgroundLoop.sh
+++ changebackgroundLoop.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #Defaults
-SETTINGSFILE="Settings.txt"
-# SLEEPTIME=15
 CUR_DIR=$( dirname ${BASH_SOURCE[0]})
+SETTINGSFILE="$CUR_DIR/Settings.txt"
+# SLEEPTIME=15
 IMAGE_DIR="$CUR_DIR/$(sed -n '2p' "$SETTINGSFILE")"
 SLEEPTIME=$(($(sed -n '4p' "$SETTINGSFILE")))
 FIRSTFILE="$IMAGE_DIR/$(sed -n '6p' "$SETTINGSFILE")"
@@ -11,7 +11,7 @@ gsettings set org.gnome.desktop.background picture-uri-dark "$FIRSTFILE"
 
 while true; do
 	sleep $((SLEEPTIME))
-	./change-background.sh
+	./$CUR_DIR/change-background.sh
 done
 exit
 
